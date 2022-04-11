fad2dbc12645b41ae5dd24d45b19fc41e787c7c4
Derrick Hinh <derhin1@gmail.com>
2022-04-11 13:02:14 -0500

    Login now works

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,69 @@
-import React, {useState} from "react"; 
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import { login } from "../api";
 
-    const Login = ({username, setUsername, password, setPassword, loginState, setLoginState}) => {
-    const [displayError, setDisplayError ] = useState (false)
+const Login = ({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  setLoginState,
+}) => {
+  const history = useHistory();
+  const [displayError, setDisplayError] = useState(false);
 
-    async function loginToken(){
-        const response = await login (username, password)
-        {response.data.token ? localStorage.setItem ( 'token', response.data.token ) : null}
+  async function loginToken() {
+    const response = await login(username, password);
+    {
+      response.token ? localStorage.setItem("token", response.token) : null;
     }
-    async function loginValid(){
-        const response = await login (username, password)
-        if (response.user){
-
-        }
+    setLoginState(true);
+  }
+  async function loginValid() {
+    const response = await login(username, password);
+    if (response.user) {
+      loginToken();
+      setDisplayError(false);
+      history.push("/home");
+      setUsername("");
+      setPassword("");
+    } else {
+      setLoginState(false);
+      setDisplayError(true);
     }
-    return (
-        <>
-        <form onSubmit={(event) => {
-            event.preventDefault();
-
-        }}>
-              <input type ='text' value={username} onChange={(event)=>{setUsername(event.target.value)}} placeholder="Username"></input>
-              <input type ='text' value={password} onChange={(event)=>{setPassword(event.target.value)}} placeholder="Password"></input>
-             <button type="submit"> Login </button>
-             
-          </form>
-         <Link to="./Register">Don't have an account? Sign Up!</Link>
-         </>
-    )
-}
+  }
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          loginValid();
+        }}
+      >
+        <input
+          type="text"
+          value={username}
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+          placeholder="Username"
+        ></input>
+        <input
+          type="text"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          placeholder="Password"
+        ></input>
+        <button type="submit"> Login </button>
+        {displayError ? (
+          <div> Wrong username and/or password. Try again.</div>
+        ) : null}
+      </form>
+      <Link to="./Register">Don't have an account? Sign Up!</Link>
+    </>
+  );
+};
 
 export default Login;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
-const Navbar = () => {
-  
+const Navbar = ({ loginState, setLoginState }) => {
+  const history = useHistory();
 
   return (
     <div id="navbar">
       <h2> Fitness Tracker </h2>
-      <Link to="../Home">
-        Home
-      </Link>
-      <Link to="../Routines">
-        Routines
-      </Link>
-      <Link to="../MyRoutines">
-        My Routines
-      </Link>
-      <Link to="../Activities">
-        Activities
-      </Link>
-      <Link to="../Login">
-        Login
-      </Link>
+      <Link to="../Home">Home</Link>
+      <Link to="../Routines">Routines</Link>
+      <Link to="../MyRoutines">My Routines</Link>
+      <Link to="../Activities">Activities</Link>
+      {loginState ? null : <Link to="../Login">Login</Link>}
+      {loginState ? (
+        <form
+          onSubmit={() => {
+            setLoginState(false);
+            localStorage.clear();
+            history.push("/home");
+          }}
+        >
+          <button type="submit"> Sign Out</button>
+        </form>
+      ) : null}
     </div>
   );
 };
