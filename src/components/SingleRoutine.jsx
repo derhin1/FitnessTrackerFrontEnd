12362eeb263fd95d5ec6250d98340764f6c009cf
Derrick Hinh <derhin1@gmail.com>
2022-04-11 13:48:03 -0500

    get all routines shows up

---
+++ src/components/SingleRoutine.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const SingleRoutine = ({ routine }) => {
+  return (
+    <div>
+      <h3>{routine.name}</h3>
+      <span>Goal: {routine.goal}</span>
+      <span>By: {routine.creatorName}</span>
+      <span>
+        Activities:{" "}
+        {routine.activities.map((activity) => {
+          console.log(activity);
+          return (
+            <div>
+              <span>Name: {activity.name}</span>
+              <span>Description: {activity.description}</span>
+              <span>Duration: {activity.duration}</span>
+              <span>Count: {activity.count}</span>
+            </div>
+          );
+        })}
+      </span>
+    </div>
+  );
+};
+
+export default SingleRoutine;
