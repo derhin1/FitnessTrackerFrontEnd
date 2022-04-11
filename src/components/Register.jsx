6d5c682e36b157d5d6fb6972c129f76e571d9f58
pam <take_asi@hotmail.com>
2022-04-11 10:36:47 -0500

    regitration works

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,7 +3,23 @@ import { register } from "../api";
 
 const Register = ({username, setUsername, password, setPassword}) => {
     const [confirmPassword, setConfirmPassword] = useState('')
-    
+    const [validated, setValidated] = useState(false)
+    const [passwordLength, setPasswordLength] = useState(false)
+
+    function valid (){
+      if (password.length >= 8){
+        setPasswordLength (false)
+        if (password === confirmPassword){
+          setValidated (false)
+          getToken ()
+        }else{
+          setValidated (true)
+        }
+      }else{
+        setPasswordLength (true)
+      }
+    }
+
     async function getToken() {
     console.log(username, "username")
     console.log(password, "password")
@@ -19,12 +35,14 @@ const Register = ({username, setUsername, password, setPassword}) => {
     return (
         <form onSubmit={(event) => {
           event.preventDefault();
-          getToken();
+          valid();
         }}>
             <input type ='text' value={username} onChange={(event)=>{setUsername(event.target.value)}} placeholder="Username"></input>
             <input type ='text' value={password} onChange={(event)=>{setPassword(event.target.value)}} placeholder="Password"></input>
             <input type ='text' value={confirmPassword} onChange={(event)=>{setConfirmPassword(event.target.value)}} placeholder="Confirm Password"></input>
            <button type="submit"> Register </button>
+           {validated ? <div>Password don't match</div> :  null }
+           {passwordLength ? <div> Password is too short</div> : null}
         </form>
     )
 }
